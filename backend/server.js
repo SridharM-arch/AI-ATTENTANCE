fd3236ac24cbced482850f764a4b7db9a95ca6d3
SridharM-arch <www.sridhar8526@gmail.com>
2026-04-11 11:25:06 +0530

    backend ready for render deploy

--- backend/server.js
+++ backend/server.js
@@ -364,7 +364,7 @@ app.get("/", (req, res) => {
 
 /* ---------------- START SERVER ---------------- */
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
